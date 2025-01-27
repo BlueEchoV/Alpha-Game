20f1a120905bf4155fd8ebbafae5d39b12d89c27
Michael <michaelepettric@gmail.com>
2025-01-26 21:51:25 -0800

    Minor fix to the hypotenuse function

--- src/Game.cpp
+++ src/Game.cpp
@@ -319,7 +319,7 @@ void draw_debug_info(Game_Data& game_data, Font& font, MP_Texture* debug_texture
 	if (Globals::debug_show_coordinates) {
 		V2 mouse = get_mouse_position(renderer->open_gl.window_handle);
 		debug_draw_coor_cs(CT_Green, true, game_data.camera.pos_ws, (int)mouse.x, (int)mouse.y, false);
-		debug_draw_coor_cs(CT_Green, true, game_data.camera.pos_ws, (int)game_data.player.pos.x, (int)game_data.player.pos.y, false);
+		debug_draw_coor_cs(CT_Green, true, game_data.camera.pos_ws, (int)game_data.player.pos.x, (int)game_data.player.pos.y, true);
 	}
 
 	mp_set_texture_color_mod(font.image.texture, 255, 255, 255);
