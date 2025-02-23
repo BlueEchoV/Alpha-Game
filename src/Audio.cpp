277ac105bb652f640d6cc12154ef205b8414c6dc
Michael <michaelepettric@gmail.com>
2025-02-23 00:19:37 -0800

    Began implementation and exploratory research of developing my own audio library.

---
+++ src/Audio.cpp
@@ -0,0 +1,5 @@
+#include "Audio.h"
+
+void test() {
+	HRESULT temp = CoCreateInstance();
+}
---
+++ src/Audio.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <Audioclient.h>
+
+// ***My own custom audio library***
+
+// Rendering a stream example:
+// https://learn.microsoft.com/en-us/windows/win32/coreaudio/rendering-a-stream
+
+
