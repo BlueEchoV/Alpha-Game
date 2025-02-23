39b3e4de0375ce0171a1065ff22f4fe792772cf5
Michael <michaelepettric@gmail.com>
2025-02-23 00:34:49 -0800

    More work researching and experimenting with the audio library.

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -1,5 +1,33 @@
 #include "Audio.h"
 
+// void mp_init_audio() {
+// 	Initialize();
+// }
+
+const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+const IID IID_IAudioClient = __uuidof(IAudioClient);
+const IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
+
+void play_audio_stream() {
+	HRESULT error_codes;
+
+	// This creates an instance of the device enumerator, 
+	// which allows the program to find audio devices.
+	error_codes = CoCreateInstance(
+		CLSID_MMDeviceEnumerator, 
+		NULL,
+		CLSCTX_ALL, 
+		IID_IMMDeviceEnumerator,
+		(void**)&pEnumerator
+	);
+	// TODO: Error check here
+
+}
+
+// void mp_play_audio() 
+// void mp_stop_audio() 
+
 void test() {
-	HRESULT temp = CoCreateInstance();
+	// HRESULT temp = CoCreateInstance();
 }
