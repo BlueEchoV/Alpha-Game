d97485050e4ede7c9be77c4e687b60c7c9f7004a
Michael <michaelepettric@gmail.com>
2025-03-31 16:36:56 -0700

    Began my implementation of the direct sound audio library.

--- src/Audio_DirectSound.h
+++ src/Audio_DirectSound.h
@@ -1,3 +1,11 @@
 #pragma once
 
-// Beginning of DirectSound
+// Beginning of DirectSoun
+// Review:
+
+// SAMPLE: In audio programming, a sample is a tiny snapshot of sound taken 
+//		   at a specific moment in time. Think of it like a single frame in a 
+//		   movie—it’s just one small piece of the audio that, when combined with 
+//		   many other samples, creates the full sound you hear, like a song or a voice.
+
+// 1) Have a buffer to store the sound
