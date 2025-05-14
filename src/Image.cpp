44535cffcd9f5a7a6ec0d55190554951d482ee14
Michael <michaelepettric@gmail.com>
2025-05-14 12:02:51 -0700

    Changed the string key to a string instead of enum

--- src/Image.cpp
+++ src/Image.cpp
@@ -114,16 +114,16 @@ void load_images() {
 	dummy_image = load_image("assets\\dummy_image.png");
 	assert(&dummy_image != NULL);
 
-	images[IT_Sun] = load_image("assets\\sun.png");
-	images[IT_Dummy_Player] = load_image("assets\\dummy_player.png");
-	images[IT_Player_Rugged_Male] = load_image("assets\\player_rugged_male.png");
-	images[IT_Enemy_Clothed_Zombie] = load_image("assets\\enemy_clothed_zombie.png");
-	images[IT_Top_Down_Player_Demo] = load_image("assets\\top_down_player_demo.png");
-	images[IT_Arrow_1] = load_image("assets\\arrow_1.png");
-	images[IT_Dummy_Tile_32x32] = load_image("assets\\dummy_tile_32x32.png");
-	images[IT_Rock_32x32] = load_image("assets\\rock_32x32.png");
-	images[IT_Grass_32x32] = load_image("assets\\grass_32x32.png");
-	images[IT_Water_32x32] = load_image("assets\\water_32x32.png");
+	images["IT_Sun"] = load_image("assets\\sun.png");
+	images["IT_Dummy_Player"] = load_image("assets\\dummy_player.png");
+	images["IT_Player_Rugged_Male"] = load_image("assets\\player_rugged_male.png");
+	images["IT_Enemy_Clothed_Zombie"] = load_image("assets\\enemy_clothed_zombie.png");
+	images["IT_Top_Down_Player_Demo"] = load_image("assets\\top_down_player_demo.png");
+	images["IT_Arrow_1"] = load_image("assets\\arrow_1.png");
+	images["IT_Dummy_Tile_32x32"] = load_image("assets\\dummy_tile_32x32.png");
+	images["IT_Rock_32x32"] = load_image("assets\\rock_32x32.png");
+	images["IT_Grass_32x32"] = load_image("assets\\grass_32x32.png");
+	images["IT_Water_32x32"] = load_image("assets\\water_32x32.png");
 }
 
 Image* get_image(std::string image_name) {
